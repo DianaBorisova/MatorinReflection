1c96ee7d7f587d464ea739ee8e78b68e696a2202
DianaBorisova <borisova@phystech.edu>
2017-12-18 17:47:21 +0300

    Initial commit

---
+++ src/homework/TestApplication.java
@@ -0,0 +1,15 @@
+package homework;
+
+public class TestApplication {
+    public static void main(String[] args) {
+        TestClass a = new TestClass(20, "name", 30.0);
+        TestClass b = new TestClass();
+        System.out.println(b.getAge());
+        System.out.println(b.getName());
+        System.out.println(b.getPrice());
+        BeanUtils.assign(b, a);
+        System.out.println(b.getAge());
+        System.out.println(b.getName());
+        System.out.println(b.getPrice());
+    }
+}
